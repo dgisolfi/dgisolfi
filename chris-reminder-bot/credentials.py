fcab00c3573ed9c98b825b0cf6cc430563b8765f
dgisolfi <daniel.gisolfi1@marist.edu>
2017-02-13 15:47:01 -0500

    created a bot to message reminders via text

---
+++ chris-reminder-bot/credentials.py
@@ -0,0 +1,9 @@
+#Prj: Marty SMS
+#Author: Daniel Gisolfi
+#Date: 2.6.17
+#credentials.py
+
+account_sid = 'ACa0d22c02571056163c578e25396cb680'
+auth_token = '5c790b2a954b39e4291df7f0b79769ce'
+my_cell = +19145392691
+my_twilio = +18458672441
