08b99b409d862c7f379a2377612a7701f723d07d
dgisolfi <daniel.gisolfi1@marist.edu>
2017-02-14 19:00:23 -0500

    atrted working on graphics for game, canvas complete

--- 2DSpace/2DSpace.py
+++ 2DSpace/2DSpace.py
@@ -5,10 +5,32 @@
 
 from Tkinter import *
 
-main = Tk()
+root = Tk()
+root.title("2D Space")
+root.wm_iconbitmap("spaceIcon.ico")
+canvas = Canvas(root, width=300, height=200)
+canvas.configure(background="black")
+
+#create player object
+
+canvas.create_rectangle(0,0,20,20, fill="white")
+
+
+
+def move():
+	x = 150
+	y = 100
+	canvas.create_rectangle(x,y,20,20, fill="white")
+	root.update()
+
+
+def quit(event):
+	#quit program
+	root.quit()
 
 def upKey(event):
-	print('uppy')
+	print("uppy")
+	move()
 
 def downKey(event):
 	print "down key pressed"
@@ -19,16 +41,28 @@ def leftKey(event):
 def rightKey(event):
     print "Right key pressed"
 
-frame = Frame(main, width=100, height=100)
+#frame = Frame(root, width=300, height=200)
 #bind keys to there corosponding methods
-main.bind('<Up>', upKey)
-main.bind('<Down>', downKey)
-main.bind('<Left>', leftKey)
-main.bind('<Right>', rightKey)
+root.bind("<q>", quit)
+root.bind("<Up>", upKey)
+root.bind("<Down>", downKey)
+root.bind("<Left>", leftKey)
+root.bind("<Right>", rightKey)
 
-#Pack Frame and call main
-frame.pack()
-main.mainloop()
-		
+
+
+#canvas.create_rectangle(0,0,150,150, fill="yellow")
+
+
+
+#Pack Frame and call root
+#frame.pack()
+canvas.pack()
+root.mainloop()
+
+
+
+def main():
+	move()
 
 
