c42624e62d7099caa7d3960fb68734e343a914b9
dgisolfi <daniel.gisolfi1@marist.edu>
2017-02-21 20:22:20 -0500

    added the ability to render other objects

--- 2DSpace/2DSpace.py
+++ 2DSpace/2DSpace.py
@@ -4,6 +4,7 @@
 # 2DSpace.py
 
 from Tkinter import *
+from random import*
 
 root = Tk()
 root.title("2D Space")
@@ -17,57 +18,71 @@ def start():
 	#||||||||||||||||||||||(x1,y1,x2,y2,)
 	global player
 	player = canvas.create_rectangle(290,290,310,310, fill="black")
-	
-
+	#mapRender()
+start()
 
 #create player object
 def move(direct):
+	global x, y
 	x = 0
 	y = 0
 	if direct == "up":
-		y = -20
+		y += -20
 	elif direct == "down":
-		y = 20
+		y += 20
 	elif direct == "left":
-		x = -20
+		x += -20
 	elif direct == "right":
-		x = 20
-	canvas.move(player, x, y)	
+		x += 20
+	#print(x,y)
+	canvas.move(player, x, y)
+def shoot():
+	pass
 
-def gameLoop():
-	
-	def quit(event):
-		#quit program
-		root.quit()
+def mapRender():
+	color = ["red", "orange", "yellow", "green", "blue", "violet", "gray", "firebrick", "dark turquoise"]
+	global powerup
+	#for i in range(25):
+	x1 = random() * 580
+	y1 = random() * 580
+	x2 = x1 - 20
+	y2 = y1 - 20
 
-	def upKey(event):
-		move("up")
+	powerup = canvas.create_rectangle(x1,y1,x2,y2, fill = choice(color))
 
-	def downKey(event):
-		move("down")
 
-	def leftKey(event):
-		move("left")
 
-	def rightKey(event):
-		move("right")
+def quit(event):
+	#quit program
+	root.quit()
 
-		#bind keys to there corosponding methods
-	root.bind("<q>", quit)
-	root.bind("<Up>", upKey)
-	root.bind("<Down>", downKey)
-	root.bind("<Left>", leftKey)
-	root.bind("<Right>", rightKey)
+def upKey(event):
+	move("up")
+
+def downKey(event):
+	move("down")
+
+def leftKey(event):
+	move("left")
+
+def rightKey(event):
+	move("right")
+
+def space(event):
+	mapRender()
+
+	#bind keys to there corosponding methods
+root.bind("<q>", quit)
+root.bind("<Up>", upKey)
+root.bind("<Down>", downKey)
+root.bind("<Left>", leftKey)
+root.bind("<Right>", rightKey)
+root.bind("<space>", space)
 
 	#Pack canvas and call root
-	canvas.pack()
-	root.mainloop()
+canvas.pack()
+root.mainloop()
 
-
-def main():
-	start()
-	gameLoop()
-main()
 
 
 
