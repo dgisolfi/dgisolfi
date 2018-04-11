cc2f6ba1f2678adfb0252f8abd6a210190e8f292
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2018-04-11 18:58:28 -0400

    updated script for new url's as of spring 2018

--- MaristRegistrationScript/RegistrationScript.js
+++ MaristRegistrationScript/RegistrationScript.js
@@ -1,30 +1,35 @@
 // ==UserScript==
-// @name Super Registration Sniper
-// @namespace https://github.com/dgisolfi/dgisolfi/blob/master/MaristRegistrationScript/MaristRegistrationScript.md
-// @version 2.1.2
-// @description Registers user for classes in under a second
-// @include https://ssb1.banner.marist.edu:8012/MARPROD/bwskfreg.P_AltPin
-// @copyright 2012+, Evan Hopkins, Antony Liang, Bob Nisco
+// @name         Super Registration Sniper
+// @namespace    http://www.google.com/
+// @version      2.1.1
+// @description  Registers user for classes in under a second
+// @include      https://ssb1.banner.marist.edu:8012/*
+// @copyright    2012+, Evan Hopkins, Antony Liang, Bob Nisco
 // ==/UserScript==
-//USAGE: Add in the CRN numbers of your classes into the crns array on line 25.
-// Load script on the 'Student' tab of the 'Banner Self Service' tab
-// of My Marist. The script will launch upon clicking the 'add or drop classes' option.
-//verifies user is on pre-registration page and then adds CRN's then submits form
 
+//INSTALLATION: Use either Greasemonkey for Firefox or Tampermonkey for Chrome to run the script.
+//              Click new script. Copy and paste the entire text of this document.
+
+//USAGE: Add in the CRN numbers of your classes into the crns array on line 25.
+//       Load script on the 'Student' tab of the 'Banner Self Service' tab
+//       of My Marist. The script will launch upon clicking the 'add or drop classes' option.
+
+//verifies user is on pre-registration page and then adds CRN's then submits form
 if (window.location == 'https://ssb1.banner.marist.edu:8012/MARPROD/bwskfreg.P_AltPin') {
-	// continuously refreshes the page until the textfields are there
-	if (document.getElementById('crn_id1') === null) {
-		location.reload();
-	} else {
-		// Put your CRNS in this array
-		var crns = ["00000" , "11111" , "22222" , "33333" , "44444", "55555"];
-		for (var i = 0; i < crns.length; i++) {
-		document.getElementById('crn_id' + (i + 1)).value = crns[i];
-	}
-	//submits form
-	document.getElementsByName('REG_BTN')[1].click();
-}
-} else if(window.location == 'https://ssb1.banner.marist.edu:8012/MARPROD/bwckcoms.P_Regs') {
-	alert("Success!");
+    // continuously refreshes the page until the textfields are there
+    if (document.getElementById('crn_id1') === null) {
+        location.reload();
+    } else {
+        // Put your CRNS in this array
+        var crns = ["00000" , "11111" , "22222" , "33333" , "44444", "55555"];
+        for (var i = 0; i < crns.length; i++) {
+            document.getElementById('crn_id' + (i + 1)).value = crns[i];
+        }
+
+        //submits form
+        document.getElementsByName('REG_BTN')[1].click();
+    }
+} else if(window.location == 'https://ssb.banner.marist.edu/PROD11G/bwckcoms.P_Regs') {
+    alert("Success!");
 }
 return;
